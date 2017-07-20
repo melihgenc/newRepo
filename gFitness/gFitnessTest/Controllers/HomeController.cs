0c24f64d15e8906e2069e98b7aa9b181f89e0b0b
utkubastug <kemalutku.bastug@std.yeditepe.edu.tr>
2017-07-20 15:50:24 +0300

    gFitness

---
+++ gFitness/gFitnessTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using gFitnessTest.Security;
+using System.Web.Mvc;
+
+namespace gFitnessTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
